89a2cc0a6aaf38f5f064aa142ea6fdb8373033a3
Jake Elwes <me@jakeelwes.com>
2020-11-18 22:12:12 +0000

    intro

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: "zizi",
+      mode: "intro",
       chosenSong: "iam",
       chosenPerformer: "amalgam",
       source: "enter",
