512afff42ef5a388b68cc90cbca659e2c66d3530
Jake Elwes <me@jakeelwes.com>
2021-03-03 17:17:46 +0000

    removed local ref

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode: "picker",
+      mode: "intro",
       chosenSong: "mylife",
       chosenPerformer: "amalgam",
       source: "enter",
